642b5ad583d7c7a9d19357d1efeac29ce2e9d923
Dante <estebanpinzon015@hotmail.com>
2025-03-12 23:05:23 -0500

    tutorial de youtube fundamentos kotlin

---
+++ app/src/main/java/com/dante/androidmaster/Variables.kt
@@ -0,0 +1,83 @@
+package com.dante.androidmaster
+
+//VARIABLES
+/**
+ * Variables númericas
+ */
+
+fun main() {
+
+
+    /**
+     * Variables alfanúmericas
+     */
+
+    //Char
+    val charExample: Char = 'e'
+    val charExample2: Char = '2'
+    val charExample3: Char = '@'
+
+    //String
+    val stringExample: String = "Holiwi ontoni"
+    var stringConcatenado:String = "Hola"
+    stringConcatenado = "Hola me llamo $stringExample y tengo $age de edad"
+    println(stringConcatenado)
+    val example123:String = age.toString()
+
+
+    /**
+     * Variables booleanas
+     */
+
+    val booleanExample:Boolean = true
+    val booleanExample2:Boolean = false
+
+    /**
+     * Variables booleanas
+     */
+
+
+
+
+}
+
+
+fun variablesNumericas(){
+    // val name = "Danielorzt
+    // 2,147,483,647 +-
+    val age: Int = 30
+    var age2: Int = 30
+
+    age2 = 29
+
+
+
+    //Long
+    val example: Long = 30
+
+
+    //Float
+    val floatExample: Float = 30.5f
+
+    //Double
+    val doubleExample: Double = 321.4311323
+
+
+//    println("Sumar:")
+//    println(age+age2)
+//
+//    println("Restar:")
+//    println(age-age2)
+//
+//    println("Multiplicar:")
+//    println(age*age2)
+//
+//    println("Divi:")
+//    println(age/age2)
+//
+//    println("Modulo:")
+//    println(age%age2)
+
+    var exampleSuma = age +floatExample.toInt()
+
+}
